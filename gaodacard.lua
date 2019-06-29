96ccab408c7f9a4ea46892b1fb4815e2e3f50de7
wch5621628 <wchwchwchw@hotmail.com>
2019-06-29 16:41:12 +0800

    Add files via upload

--- gaodacard.lua
+++ gaodacard.lua
@@ -281,7 +281,11 @@ Guard_skill = sgs.CreateTriggerSkill{
 	on_trigger = function(self, event, player, data)
 		local room = player:getRoom()
 		local damage = data:toDamage()
-		if damage.card and (damage.card:isKindOf("Slash") or damage.card:isKindOf("Shoot") and damage.card:objectName() ~= "pierce_shoot") then
+		if damage.card and (damage.card:isKindOf("Slash") or string.find(damage.card:objectName(), "shoot") and damage.card:objectName() ~= "pierce_shoot") then
+			
+			--已挡
+			if damage.card:hasFlag("Guard") then return false end
+			
 			local guard
 			if damage.from then
 				guard = room:askForCard(player, "Guard", "@Guard:"..damage.card:objectName()..":"..damage.from:objectName(), sgs.QVariant(), sgs.Card_MethodUse, damage.from)
@@ -290,7 +294,7 @@ Guard_skill = sgs.CreateTriggerSkill{
 			end
 			if guard then
 				math.random()
-				if (damage.card:isKindOf("Shoot") or math.random(1, 100) <= 70) then
+				if (string.find(damage.card:objectName(), "shoot") or math.random(1, 100) <= 70) then
 					local log = sgs.LogMessage()
 					log.type = "#burstd"
 					log.to:append(damage.to)
